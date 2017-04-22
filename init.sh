02330471fd78c0956ee8d7ca40bd25ffe7df8f54
Denis Sidorenko <denisissveta@mail.ru>
2017-04-22 20:39:58 +0300

    First working with Django application

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/gunicorn-2.conf /etc/gunicor.d/test.py
 sudo /etc/init.d/gunicorn restart
