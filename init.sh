31c962a39cb7b26d35612f7d6ea8369a716d0b60
qoffee <denisissveta@mail.ru>
2017-04-29 01:16:12 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
+# sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+# sudo /etc/init.d/nginx restart
+# sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
+# sudo ln -sf /home/box/web/etc/gunicorn-2.conf /etc/gunicor.d/hello-2.py
+# sudo /etc/init.d/gunicorn restart
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/gunicorn-2.conf /etc/gunicor.d/hello-2.py
+sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn-2.conf  /etc/gunicorn.d/test2
 sudo /etc/init.d/gunicorn restart
