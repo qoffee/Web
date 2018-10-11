c930c48d3aebdf0fbcd49f8fb2316f7036497d32
qoffee <denisissveta@mail.ru>
2018-10-11 14:56:08 +0300

    changes in init.sh2

--- init.sh
+++ init.sh
@@ -1,16 +1,16 @@
-# sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-# sudo /etc/init.d/nginx restart
-# sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
-# sudo ln -sf /home/box/web/etc/gunicorn-2.conf /etc/gunicor.d/hello-2.py
-# sudo /etc/init.d/gunicorn restart
-# sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-# sudo /etc/init.d/nginx restart
-# sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
-# sudo ln -sf /home/box/web/etc/gunicorn-2.conf  /etc/gunicorn.d/test2
-# sudo /etc/init.d/gunicorn restart
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/Web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/Web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
-sudo ln -sf /home/box/Web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/gunicorn-2.conf /etc/gunicor.d/hello-2.py
 sudo /etc/init.d/gunicorn restart
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/gunicorn-2.conf  /etc/gunicorn.d/test2
+sudo /etc/init.d/gunicorn restart
+# sudo rm /etc/nginx/sites-enabled/default
+# sudo ln -sf /home/box/Web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+# sudo /etc/init.d/nginx restart
+# sudo ln -sf /home/box/Web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+# sudo ln -sf /home/box/Web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+# sudo /etc/init.d/gunicorn restart
